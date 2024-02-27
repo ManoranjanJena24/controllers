50a8ca07de9e05e5f155ce087910c2b8537ed70a
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-02-27 16:38:57 +0530

    Models

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,4 @@
-const products = [];
-
-
+const Product = require('../models/product')
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
         pageTitle: 'Add Product',
@@ -13,11 +11,15 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title) 
+    product.save();
     res.redirect('/');
+    
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = new Product.fetchAllProducts();
+    
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
